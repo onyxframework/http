2feb50eb28ffceabfb272f3761bf3757a80f6ed3
Vlad Faust <mail@vladfaust.com>
2018-04-26 20:06:57 +0300

    doc: remove #from_s docs

--- src/prism/ext/from_s.cr
+++ src/prism/ext/from_s.cr
@@ -1,39 +1,46 @@
+# :nodoc:
 struct Float32
   def self.from_s(string)
     string.to_f32
   end
 end
 
+# :nodoc:
 struct Float64
   def self.from_s(string)
     string.to_f64
   end
 end
 
+# :nodoc:
 struct Int32
   def self.from_s(string)
     string.to_i32
   end
 end
 
+# :nodoc:
 struct Int64
   def self.from_s(string)
     string.to_i64
   end
 end
 
+# :nodoc:
 struct Time
   def self.from_s(string)
     epoch(string.to_i64)
   end
 end
 
+# :nodoc:
 struct Time::Span
   def self.from_s(string)
     new(string.to_i64 * TicksPerSecond)
   end
 end
 
+# :nodoc:
 class String
   def self.from_s(string)
     string
